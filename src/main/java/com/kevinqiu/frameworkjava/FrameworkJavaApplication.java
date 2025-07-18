85e77e65b58ebfef16667c568afe665e7a676ccb
Kevin Qiu <ess102692@gmail.com>
2025-07-18 18:19:12 +0800

    first commit

---
+++ src/main/java/com/kevinqiu/frameworkjava/FrameworkJavaApplication.java
@@ -0,0 +1,13 @@
+package com.kevinqiu.frameworkjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kevinqiu/frameworkjava/FrameworkJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinqiu.frameworkjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
