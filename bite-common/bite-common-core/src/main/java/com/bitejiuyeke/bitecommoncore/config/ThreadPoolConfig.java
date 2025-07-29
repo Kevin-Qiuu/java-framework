b8a5b3a5239a81a9a112e219e31bee6664ddf24d
Kevin Qiu <ess102692@gmail.com>
2025-07-29 21:40:59 +0800

    feat(bite-common-core): 添加自定义线程池配置
    - 新增 ThreadPoolConfig 类，用于配置自定义线程池
    - 添加 RejectExecutionType 枚举类，定义线程池拒绝策略
    - 在 resources 目录下添加 AutoConfiguration.imports 文件，引入自动配置

---
+++ bite-common/bite-common-core/src/main/java/com/bitejiuyeke/bitecommoncore/config/ThreadPoolConfig.java
@@ -0,0 +1,82 @@
+package com.bitejiuyeke.bitecommoncore.config;
+
+import com.bitejiuyeke.bitecommoncore.enums.RejectedExecutionType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+// @EnableAsync 注解会与 @Async 注解搭配使用
+// 只有被 @EnableAsync 注解修饰的线程池对象才可以接受 @Async 修饰的方法任务
+// 否则提交到 Spring 的默认线程池中
+@EnableAsync
+@Configuration
+public class ThreadPoolConfig {
+
+    /**
+     * 核心线程数
+     */
+    @Value("${thread.pool-executor.corePoolSize:5}")
+    private Integer corePoolSize;
+
+    /**
+     * 最大线程数
+     */
+    @Value("${thread.pool-executor.maxPoolSize}")
+    private Integer maxPoolSize;
+
+    /**
+     * 阻塞队列容量
+     */
+    @Value("${thread.pool-executor.queueCapacity}")
+    private Integer queueCapacity;
+
+    /**
+     * 拒绝策略类型
+     */
+    @Value("${thread.pool-executor.rejectedExecutionType}")
+    private Integer rejectedExecutionType;
+
+    /**
+     * 线程名称前缀
+     */
+    @Value("${thread.pool-executor.threadNamePrefix}")
+    private String threadNamePrefix;
+
+    @Bean("threadPoolTaskExecutor")
+    public ThreadPoolTaskExecutor getThreadPoolTaskExecutor() {
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setRejectedExecutionHandler(getRejectedExecutionHandler(rejectedExecutionType));
+        executor.setThreadNamePrefix(threadNamePrefix);
+
+        return executor;
+
+    }
+
+    private RejectedExecutionHandler getRejectedExecutionHandler(Integer rejectedExecutionType) {
+
+        RejectedExecutionType type = RejectedExecutionType.forValue(rejectedExecutionType);
+
+        if (null == type) {
+            return new ThreadPoolExecutor.CallerRunsPolicy();
+        }
+
+        return switch (type) {
+            case AbortPolicy -> new ThreadPoolExecutor.AbortPolicy();
+            case CallerRunsPolicy -> new ThreadPoolExecutor.CallerRunsPolicy();
+            case DiscardPolicy -> new ThreadPoolExecutor.DiscardPolicy();
+            case DiscardOldestPolicy -> new ThreadPoolExecutor.DiscardOldestPolicy();
+        };
+
+    }
+
+}
