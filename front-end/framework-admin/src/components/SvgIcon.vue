31f1fc3d583ad5e58fea821d67e626c859a0e0e1
Kevin Qiu <ess102692@gmail.com>
2025-10-27 21:19:03 +0800

    上传了前端源码🕶

---
+++ front-end/framework-admin/src/components/SvgIcon.vue
@@ -0,0 +1,33 @@
+<script setup>
+  const props = defineProps({
+    // 图标名称
+    name: String,
+    // 图标颜色
+    color: String,
+    // 图标大小
+    size: {
+      // 字符串或数字类型
+      type: [String, Number],
+      // 默认值
+      default: 1
+    }
+  })
+</script>
+<script>
+  export default {
+    name: 'SvgIcon'
+  }
+</script>
+<template>
+  <svg
+    aria-hidden="true"
+    :style="{
+      width: props.size + 'em',
+      height: props.size + 'em'
+    }"
+  >
+    <use :href="`#icon-${props.name}`" :fill="props.color" />
+  </svg>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ front-end/framework-admin/src/views/House/Detail.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<script>
+  export default {
+    name: 'HouseDetai了Page'
+  }
+</script>
+<template>
+  <div>房源详情</div>
+</template>
+<style lang="scss" scoped></style>
